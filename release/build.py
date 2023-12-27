c687d94444fde487475a8527322ff86bd75c5086
LDLDL <hyx1278199910@outlook.com>
2023-12-27 06:28:16 +0000

    Add build script

---
+++ release/build.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import datetime
+
+subprocess.run("rm -r ./certdx_*", shell=True)
+
+commitId = subprocess.run("git rev-parse --short HEAD", shell=True, capture_output=True)
+commitId = commitId.stdout.decode().strip()
+
+time = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M')
+
+t = [['linux', 'amd64'],
+     ['linux', 'arm'],
+     ['linux', 'arm64'],
+     ['linux', 'mips'],
+     ['linux', 'mipsle'],
+     ['windows', 'amd64'],
+]
+
+exec = [['server',         'server'],
+        ['client',         'client'],
+        ['google_account', 'google_account'],
+]
+
+for o, a in t:
+    dir = f'certdx_{o}_{a}'
+    for e, s in exec:
+        subprocess.run(
+            f'''env GOOS="{o}" GOARCH="{a}" '''
+            f'''go build -ldflags="-s -w '''
+            f'''-X main.buildCommit={commitId} -X \'main.buildDate={time}\'" '''
+            f'''-o {dir}/{e}{".exe" if o == "windows" else ""} '''
+            f'''../exec/{s}''', shell=True
+        )
+    subprocess.run(f"zip -r {dir}.zip {dir}", shell=True)
+    subprocess.run(f"rm -r {dir}", shell=True)
+
